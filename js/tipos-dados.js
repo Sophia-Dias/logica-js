4a1bc8ab31c39b896afe44d13f7e480fd43ab446
Sophia-Dias <76136379+Sophia-Dias@users.noreply.github.com>
2021-04-13 17:08:10 -0300

    Tipos de Dados

---
+++ js/tipos-dados.js
@@ -0,0 +1,18 @@
+/* TIPOS DE DADOS */
+
+    //   - string = texto
+    //   - number = número
+    //   - boolean = verdadeiro ou falso
+    //   - object = "grupo de váriaveis"
+    //   - array = "grupo de objetos"
+    //   - undefined = valor indefinido
+    //   - NaN = espera número, mas vem outro valor = Not a Number
+
+/* EXEMPLO */
+
+const text = "Unicorns";
+const value = 10;
+const active = false;
+const addressObj = {nome: "Unicorns", endereço: "Rua de Alguém", numero: 22};
+const addressArr = ["Unicorns", "JavaScript", "JS"];
+const addressData = [0, 1, 2, 3];
