782eab0a20dfe0c1c3d1ffd0c21f20c0231bc733
Sophia-Dias <76136379+Sophia-Dias@users.noreply.github.com>
2021-04-13 20:23:50 -0300

    Comentários em JS

---
+++ js/comentarios.js
@@ -0,0 +1,17 @@
+/* COMENTÁRIOS EM JAVASCRIPT */
+
+    // Comentário Simples
+        /*
+            Para fazer um comentário simples basta
+            adicionar duas barras "//" no começo da
+            linha.
+        */
+
+    // Comentário em Bloco
+        /*
+            Para comentar várias linha de uma só vez,
+            coloque uma barra "/" e depois um asterisco
+            "*" para comentar tudo dali em 
+            diante e para fechar coloque um asterisco "*" e depois uma barra
+            "/" para limitar as linhas comentadas.
+        */
