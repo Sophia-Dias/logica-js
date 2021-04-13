9d9f3fe8d1a1bed75657ee66994d6cc4f5197068
Sophia-Dias <76136379+Sophia-Dias@users.noreply.github.com>
2021-04-13 15:49:17 -0300

    Resumo Variaveis

---
+++ js/variaveis.js
@@ -0,0 +1,13 @@
+/* VARIAVEIS */
+
+// tipos:  var, const e let.
+
+// const = o tipo de variavel const não pode ter o valor mudado, ou seja, constante.
+
+// var = pode ter valor mudado, e pode não ter valor pré definido.
+    // sem valor pré definido (var nome);
+    // com valor pré definido (var nome = sophia);
+
+// let = pode ter valor mudado, e também nao precisa ter valor pré definido.
+    // sem valor pré definido (let nome);
+    // com valor pré definido (let nome = sophia);
