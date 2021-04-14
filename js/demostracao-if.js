1d7ee37dc8f77b10cc340b47bf611ea9b1b0c5d2
Sophia-Dias <76136379+Sophia-Dias@users.noreply.github.com>
2021-04-14 17:01:57 -0300

    Demostração do IF e modificação do index para depuração

---
+++ js/demostracao-if.js
@@ -0,0 +1,14 @@
+/* DEMOSTRAÇÃO DO IF */
+
+const a = 10
+const b = 5
+
+if (10 !== 5) {
+    console.log("IF: É verdadeiro");
+}
+else if (a > b) {
+    console.log("ELSE IF: É verdadeiro");
+}
+else {
+    console.log("ELSE: É falso");
+}
