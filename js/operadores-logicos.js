76dda726820a8f454ec86d40a346e16b8af4ed93
Sophia-Dias <76136379+Sophia-Dias@users.noreply.github.com>
2021-04-13 22:14:40 -0300

    Operadores Lógicos

---
+++ js/operadores-logicos.js
@@ -0,0 +1,15 @@
+/* OPERADORES LÓGICOS */
+
+    /*
+        ==   →  Igual            |   A == B  → Verdadeiro se A for igual a B
+        !=   →  Diferente        |   A != B  → Verdadeiro se A não for igual a B
+        ===  →  Idêntico         |   A === B → Verdadeiro se A for idêntico a B
+        !==  →  Não idêntico     |   A !== B → Verdadeiro se A não for igual a B ou eles não tiverem o mesmo tipo
+        <    →  Menor que        |   A < B   → Verdadeiro se A for menor que B
+        >    →  Maior que        |   A > B   → Verdadeiro se A for maior que B
+        <=   →  Menor ou igual   |   A <= B  → Verdadeiro se A for menor ou igual a B
+        >=   →  Maior ou igual   |   A >= B  → Verdadeiro se A for maior ou igual a B
+        &&   →  E                |   A && B  → Verdadeiro se existir A e B
+        ||   →  Ou               |   A || B  → Verdadeiro se existir A ou existir B
+        !    →  Negação          |   !a      → Negação, se o valor for verdadeiro vai transformar em falso, e vice versa
+    */
