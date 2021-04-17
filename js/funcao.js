57b4c77463387d90ef0f8fde260b075b64f326d3
Sophia-Dias <76136379+Sophia-Dias@users.noreply.github.com>
2021-04-16 23:51:22 -0300

    Funções e modificação do index

---
+++ js/funcao.js
@@ -0,0 +1,19 @@
+/* FUNÇÕES */
+
+    // Como declarar
+    function calculo(numero) {
+        // sem retorno
+        console.log(numero * numero);
+        return ;
+
+        // com retorno
+        return numero * numero;
+    }
+
+    // Como chamar
+    calculo(10)
+    
+    // Como mostrar no console
+    console.log(
+        calculo(10)       
+    );
